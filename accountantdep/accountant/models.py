921bbf7e10c2eef6fc8f678a0c9a952979892d6a
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-03-19 21:29:17 +0100

    models sketch

--- accountantdep/accountant/models.py
+++ accountantdep/accountant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Wallet(models.Model):
+    asset = models.CharField("Asset", max_length=256)
+    quantity = models.FloatField()
+
+
+class History(models.Model):
+    transaction_type = models.CharField("Transaction type", max_length=64)
+    transaction_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class Balance(models.Model):
+    pass
+
+
+class Transactions(models.Model):
+    pass
+
+
+class Types(models.Model):
+    pass
+
+
+class AssetTypes(models.Model):
+    pass
